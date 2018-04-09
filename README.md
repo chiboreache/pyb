8fea113f312665c4c66918f93e36937477ee6da8
chombor <chombor@gmail.com>
2018-04-10 01:34:57 +0300

    0.0.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Scripts
 
-## muimportFBX.py
+## [muimportFBX.py](https://github.com/chombor/pyb/blob/master/muimportFBX.py)
 
 Simple import of any *.fbx* files from the "**FBX**" folder in your *%USERNAME%/Desktop* directory
 
--- muimportFBX.py
+++ muimportFBX.py
@@ -2,9 +2,7 @@ import os
 import bpy
 import glob
 
-desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
-importDir = desktop + '\\fbx'
+os.chdir(os.path.join(os.environ['USERPROFILE'], 'Desktop', 'fbx'))
 
-os.chdir(importDir)
 for files in glob.glob('*.fbx'):
-    bpy.ops.import_scene.fbx(filepath=files, filter_glob='*.fbx;', axis_forward='-Z', axis_up='Y')
+	bpy.ops.import_scene.fbx(filepath=files, filter_glob='*.fbx;')
